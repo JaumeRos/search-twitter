52f40a733ee00ddec552d72f2a2422d2421b5294
Jaume Ros <jaumeros77@gmail.com>
2021-01-12 16:58:37 +0100

    added lil avatar image of twitter users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[twitter]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid, username: auth.info.nickname).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid, username: auth.info.nickname, image: auth.info.image).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       # user.name = auth.info.name
-      user.username = auth.info.nickname
+      user.image = auth.info.image
     end
   end
 
